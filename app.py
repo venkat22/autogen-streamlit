e08e1a61a823e8ea3ef121e8ab921102730cad13
Venkata Y <yerrapothuvenkata@johndeere.com>
2023-12-14 19:23:57 -0600

    adding Dockerfile

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import os
+
 import streamlit as st
 import asyncio
 from autogen import AssistantAgent, UserProxyAgent
+from dotenv import load_dotenv
 
+load_dotenv()
 
 st.write("""# AutoGen Chat Agents""")
+print(os.environ.get("api_key"))
+
 
 class TrackableAssistantAgent(AssistantAgent):
     def _process_received_message(self, message, sender, silent):
@@ -18,52 +24,41 @@ class TrackableUserProxyAgent(UserProxyAgent):
             st.markdown(message)
         return super()._process_received_message(message, sender, silent)
 
-
-selected_model = None
-selected_key = None
-with st.sidebar:
-    st.header("OpenAI Configuration")
-    selected_model = st.selectbox("Model", ['gpt-3.5-turbo', 'gpt-4'], index=1)
-    selected_key = st.text_input("API Key", type="password")
-
-with st.container():
     # for message in st.session_state["messages"]:
     #    st.markdown(message)
 
-    user_input = st.chat_input("Type something...")
-    if user_input:
-        if not selected_key or not selected_model:
-            st.warning(
-                'You must provide valid OpenAI API key and choose preferred model', icon="⚠️")
-            st.stop()
 
-        llm_config = {
-            "request_timeout": 600,
-            "config_list": [
-                {
-                    "model": selected_model,
-                    "api_key": selected_key
-                }
-            ]
-        }
-        # create an AssistantAgent instance named "assistant"
-        assistant = TrackableAssistantAgent(
-            name="assistant", llm_config=llm_config)
+user_input = st.chat_input("Type something...")
+if user_input:
+    llm_config = [{
+        "model": "jdcraig",
+        "api_key": os.environ.get("api_key"),
+        "base_url": "https://cgai.openai.azure.com/",
+        "api_type": "azure",
+        "api_version": "2023-07-01-preview"
+    }
+    ]
 
-        # create a UserProxyAgent instance named "user"
-        user_proxy = TrackableUserProxyAgent(
-            name="user", human_input_mode="NEVER", llm_config=llm_config)
+    # create an AssistantAgent instance named "assistant"
+    assistant = TrackableAssistantAgent(
+        name="assistant", llm_config={"config_list": llm_config})
 
-        # Create an event loop
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
+    # create a UserProxyAgent instance named "user"
+    user_proxy = TrackableUserProxyAgent(
+        name="user", human_input_mode="NEVER", llm_config={"config_list": llm_config})
 
-        # Define an asynchronous function
-        async def initiate_chat():
-            await user_proxy.a_initiate_chat(
-                assistant,
-                message=user_input,
-            )
+    # Create an event loop
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
-        # Run the asynchronous function within the event loop
-        loop.run_until_complete(initiate_chat())
+
+    # Define an asynchronous function
+    async def initiate_chat():
+        await user_proxy.a_initiate_chat(
+            assistant,
+            message=user_input,
+        )
+
+
+    # Run the asynchronous function within the event loop
+    loop.run_until_complete(initiate_chat())
